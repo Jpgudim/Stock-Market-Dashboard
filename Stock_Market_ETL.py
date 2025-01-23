1e63051c29a2a7d6a7b1dc40f585e44444d3df87
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2025-01-22 23:55:25 -0600

    Added Range and Print Function
    Added a function that will collect daily stock data from a date range. Need to clean data and add error checking for when when market is closed. Also moved print statements into a function.

--- Stock_Market_ETL.py
+++ Stock_Market_ETL.py
@@ -9,6 +9,7 @@ import pandas as pd
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -32,6 +33,30 @@ class StockMarketPipeline():
         data = daily.json()
 
         return data
+    
+    def print_daily_data(self, data):
+
+        print(f"Symbol: {data['symbol']:>13}")
+        print(f"Date: {data['from']:>15}")
+        print(f"Open: {data['open']:>15}")
+        print(f"Close: {data['close']:>14}")
+
+    def get_data_range(self, symbol, start, end):
+
+        start = datetime.strptime(start, "%Y-%m-%d")
+        end = datetime.strptime(end, "%Y-%m-%d")
+
+        current_date = start    
+        data = []
+        
+        while current_date <= end:
+            print(current_date)
+            data.append(self.fetch_daily_data(symbol, current_date.strftime("%Y-%m-%d")))
+            current_date += timedelta(days=1)
+        
+        return data
+
+        
 
 
 def main():
@@ -44,11 +69,11 @@ def main():
     
     daily_change = daily['close'] - daily['open']
 
-    print(f"Symbol: {daily['symbol']:>13}")
-    print(f"Date: {daily['from']:>15}")
-    print(f"Open: {daily['open']:>15}")
-    print(f"Close: {daily['close']:>14}")
+    pipeline.print_daily_data(daily)
+
     print (f"change: {daily_change:>13.2f}")
+
+    print(pipeline.get_data_range("AAPL", "2025-01-06", "2025-01-08"))
     
    
 if __name__ == "__main__":
